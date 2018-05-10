c0740162bdea436c21c26388ba2aa59a789bffd1
oliver <loliverhennigh101@gmail.com>
2018-05-10 16:37:44 -0500

    added everyting

--- squeeze_net/script.js
+++ squeeze_net/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 async function run_entry() {
     try {
         await run();
@@ -31,6 +32,30 @@ function displayLog() {
 async function loadImage() {
     let imageData = await WebDNN.Image.getImageArray(document.getElementById('file'), {dstW: 224, dstH: 224});
     WebDNN.Image.setImageArrayToCanvas(imageData, 224, 224, document.getElementById('input_image'));
+
+    var canvas = document.getElementById('name_image')
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    document.getElementById('run_button').disabled = false;
+    log('Image loaded to canvas');
+}
+
+
+async function loadRandImage() {
+    let min_index = Math.floor((Math.random() * imagenet_labels.length) + 1); 
+    let img_index = Math.floor((Math.random() * 4) + 0); 
+    let mineral_name = imagenet_labels[min_index]
+    let filename = '/data/' + mineral_name + '_' + ("000" + img_index).slice (-4) + '.jpg';
+    console.log(filename)
+    let imageData = await WebDNN.Image.getImageArray(filename, {dstW: 224, dstH: 224});
+    WebDNN.Image.setImageArrayToCanvas(imageData, 224, 224, document.getElementById('input_image'));
+
+    var canvas = document.getElementById('name_image')
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Arial";
+    ctx.fillText(mineral_name,10,50);
 
     document.getElementById('run_button').disabled = false;
     log('Image loaded to canvas');
@@ -64,7 +89,7 @@ async function run() {
     runner.inputs[0].set(await WebDNN.Image.getImageArray(document.getElementById('input_image'), {
         dstW: 223, dstH: 223,
         order: WebDNN.Image.Order.HWC,
-        color: WebDNN.Image.Color.BGR,
+        color: WebDNN.Image.Color.RGB,
         bias: [122.679, 116.669, 104.006] // RGB mean (not BGR)
     }));
 
@@ -73,7 +98,7 @@ async function run() {
     let elapsed_time = performance.now() - start;
 
     let out_vec = runner.outputs[0].toActual();
-    let top_labels = WebDNN.Math.argmax(out_vec, 5);
+    let top_labels = WebDNN.Math.argmax(out_vec, 10);
 
     let predicted_str = 'Predicted: <br>';
     for (let j = 0; j < top_labels.length; j++) {
