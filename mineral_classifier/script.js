29bfe4903344d887daf1622a0528892879218e80
oliver <loliverhennigh101@gmail.com>
2018-05-26 15:27:50 -0500

    new classifier

--- mineral_classifier/script.js
+++ mineral_classifier/script.js
@@ -77,7 +77,7 @@ async function prepare_run() {
     if (!(backend_key in runners)) {
         log('Initializing and loading model');
         log(`./output_${framework_name}`, {backendOrder: backend_name});
-        let runner = await WebDNN.load(`./output_new_${framework_name}`, {backendOrder: backend_name});
+        let runner = await WebDNN.load(`./output_new_${framework_name}_1`, {backendOrder: backend_name});
         log(`Loaded backend: ${runner.backendName}, model converted from ${framework_name}`);
 
         runners[backend_key] = runner;
